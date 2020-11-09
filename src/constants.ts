2f6ebc753c8c2d6221233dfb96d08fa58f56bde4
evanchristians <evanryk@gmail.com>
2020-11-09 15:49:47 +0200

    fixed dev configs

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 export const IS_PROD = process.env.ENV === "production";
 export const WS_URI = IS_PROD
   ? "wss://gql-sub.herokuapp.com/subscriptions"
-  : "ws://localhost:5000/graphql/subscriptions";
+  : "ws://localhost:5000/subscriptions";
 
 export const HTTP_URI = IS_PROD
   ? "https://gql-sub.herokuapp.com/graphql"
-  : "http://localhost:3000";
+  : "http://localhost:5000/graphql";
