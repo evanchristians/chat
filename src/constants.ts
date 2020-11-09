a7d9699b508c47982ad58a16df534a3d61485443
evanchristians <evanryk@gmail.com>
2020-11-09 15:53:31 +0200

    more configs

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 export const IS_PROD = process.env.ENV === "production";
 export const WS_URI = IS_PROD
   ? "wss://gql-sub.herokuapp.com/subscriptions"
