4f91223623cfba01a1f0e65fbf8ed697ee50a3c2
evanchristians <evanryk@gmail.com>
2020-11-11 16:17:13 +0200

    updated endpoints

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 export const IS_PROD = process.env.ENV === "production";
 export const WS_URI = IS_PROD
-  ? "wss://gql-sub.herokuapp.com/subscriptions"
+  ? "ws://gql-sub-server.evanchristians.co.za/subscriptions"
   : "ws://localhost:5000/subscriptions";
 
 export const HTTP_URI = IS_PROD
-  ? "https://gql-sub.herokuapp.com/graphql"
+  ? "http://gql-sub-server.evanchristians.co.za/graphql"
   : "http://localhost:5000/graphql";
