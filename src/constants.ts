40e0348ac557b45cdf431cbd4f5251b19381ff7c
evanchristians <evanryk@gmail.com>
2020-11-11 17:33:01 +0200

    added ssl

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 export const IS_PROD = process.env.ENV === "production";
 export const WS_URI = IS_PROD
-  ? "ws://gql-sub-server.evanchristians.co.za/subscriptions"
+  ? "wss://gql-sub-server.evanchristians.co.za/subscriptions"
   : "ws://localhost:5000/subscriptions";
 
 export const HTTP_URI = IS_PROD
-  ? "http://gql-sub-server.evanchristians.co.za/graphql"
+  ? "https://gql-sub-server.evanchristians.co.za/graphql"
   : "http://localhost:5000/graphql";
