5ba8462796a496272204745a4919583ae44cc7ef
evanchristians <evanryk@gmail.com>
2020-11-16 00:36:48 +0200

    domain update

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 export const IS_PROD = process.env.ENV === "production";
 export const WS_URI = IS_PROD
-  ? "wss://gql-sub-server.evanchristians.co.za/subscriptions"
+  ? "wss://chat-server.evanchristians.co.za/subscriptions"
   : "ws://localhost:5000/subscriptions";
 
 export const HTTP_URI = IS_PROD
-  ? "https://gql-sub-server.evanchristians.co.za/graphql"
+  ? "https://chat-server.evanchristians.co.za/graphql"
   : "http://localhost:5000/graphql";
